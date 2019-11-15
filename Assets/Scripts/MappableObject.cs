d0aaedfc3ab973667a10d9068359266eff931719
Victor <victor.s@live.dk>
2019-11-15 22:30:09 +0100

    idk

---
+++ Assets/Scripts/MappableObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MappableObject : MonoBehaviour
+{
+    GameObject vis;
+
+    void Start()
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        vis = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        vis.transform.position = transform.position;
+        vis.transform.localScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Time.time;
+        var v = Mapping.s(new Vector2(transform.position.x, transform.position.y), t);
+        vis.transform.position = new Vector3(v.x, v.y, vis.transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Mapping.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Mapping
+{
+    static float d = 1f;
+    static float a = 0.5f;
+    static int n = 5;
+    static float g = n * Mathf.PI * 2;
+    static float b = 1f;
+    static float c = 0f;
+
+    public static Vector2 s(Vector2 v, float t)
+    {
+        float s = d * v.x + (a / g) * (Mathf.Cos(t * b + c) - Mathf.Cos(t * b + c + g * v.x));
+        return new Vector2(s, v.y);
+    }
+}
